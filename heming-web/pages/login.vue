5bc736c36a45336bb6548c1dc6d7f8bd119c390f
besscroft <besscroft@foxmail.com>
2023-08-26 20:37:11 +0800

    fix:修改登录

--- heming-web/pages/login.vue
+++ heming-web/pages/login.vue
@@ -7,8 +7,8 @@ const user = useUserStore()
 const router = useRouter()
 
 const loginForm = reactive<Login.ReqLoginForm>({
-  username: '',
-  password: '',
+  username: 'view',
+  password: '666666',
 })
 
 const handleSubmitClick = async () => {
@@ -76,7 +76,6 @@ onUnmounted(() => {
                           id="exampleFormControlInput11"
                           v-model="loginForm.password" />
                     </div>
-                    预览账号：view/666666
                     <div mb-12 pb-1 pt-1 text-center>
                       <button
                           class="font-ark mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
