6a3ddbe9ba5a4f48e4461e6aa430097df621594b
besscroft <besscroft@foxmail.com>
2023-09-05 17:58:32 +0800

    style:移除不需要的样式

--- heming-web/pages/index.vue
+++ heming-web/pages/index.vue
@@ -10,23 +10,6 @@ definePageMeta({
   <div flex flex-col justify-center items-center h-full>
     <div font-ark text-5xl text-yellow-400>嗯学英语</div>
     <div font-ark text-slate-400>学英语，就嗯学！</div>
-    <div flex flex-col space-x-2 mt-24>
-      <div class="chat chat-start">
-        <div class="chat-image avatar">
-          <div class="w-10 rounded-full">
-            <img src="https://besscroft.com/uploads/avatar.jpeg" />
-          </div>
-        </div>
-        <div class="chat-header">
-          云淑
-          <time class="text-xs opacity-50">12:45</time>
-        </div>
-        <div class="chat-bubble">You were the Chosen One!</div>
-        <div class="chat-footer opacity-50">
-          已读
-        </div>
-      </div>
-    </div>
     <div font-ark mt-24 text-xl text-lime-400 cursor-pointer @click="router.push('/learn')">开始</div>
     <div flex space-x-2 mt-24>
       <DarkToggle />
