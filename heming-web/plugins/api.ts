a5f32e83bae17069d80ecc6ed4a6280708eeffa9
besscroft <besscroft@foxmail.com>
2023-09-11 16:48:44 +0800

    chore:设置请求超时时间

--- heming-web/plugins/api.ts
+++ heming-web/plugins/api.ts
@@ -2,32 +2,33 @@ import { useUserStore } from '~/stores/user'
 import ky from 'ky'
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const app = nuxtApp.vueApp;
+    const app = nuxtApp.vueApp
     const userStore = useUserStore(app.$pinia)
     const router = useRouter()
     const toast = useToast()
 
     const apiFetch = ky.extend({
+        timeout: 600000,
         hooks: {
             beforeRequest: [
                 request => {
-                    request.headers.set('Authorization', 'Bearer ' + userStore.token);
+                    request.headers.set('Authorization', 'Bearer ' + userStore.token)
                 }
             ],
             beforeError: [
                 error => {
-                    const {response} = error;
+                    const {response} = error
                     console.log(response.status);
                     if (response.status === 401) {
                         toast.add({ title: '登录已失效!', timeout: 2000, ui: { width: 'w-full sm:w-96' }})
-                        router.push('/login');
+                        router.push('/login')
                     }
-                    return error;
+                    return error
                 }
             ]
         }
     })
 
-    app.config.globalProperties.$api = apiFetch;
-    nuxtApp.provide("api", apiFetch);
-});
+    app.config.globalProperties.$api = apiFetch
+    nuxtApp.provide("api", apiFetch)
+})
